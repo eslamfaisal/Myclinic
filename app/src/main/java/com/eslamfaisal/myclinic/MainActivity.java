970f567012458a13cf510deb39e154a41b2f04f5
eslam faisal <eslamfaisal@github.com>
2018-04-06 14:14:57 +0200

    add activities

--- app/src/main/java/com/eslamfaisal/myclinic/MainActivity.java
+++ app/src/main/java/com/eslamfaisal/myclinic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.eslamfaisal.myclinic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab =  findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,CatalogActivity.class);
+                startActivity(intent);
             }
         });
     }
